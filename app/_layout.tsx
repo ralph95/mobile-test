9a59a3bb066f52f37c7d01232f864f7685bb957e
ralph95 <ralphbaylon95@gmail.com>
2026-05-04 13:41:40 +0800

    Layout: Status bar set to hidden

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import "./globals.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
